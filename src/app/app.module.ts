e8e96c4d457f4dd8d1a558ddbe119a86407cb637
NawodaRanasingha <nawodaranasinha@gmail.com>
2024-05-08 16:39:16 +0530

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { AboutComponent } from './components/about/about.component';
 import { BookingComponent } from './components/booking/booking.component';
 import { ProductsComponent } from './components/products/products.component';
 import { CommentComponent } from './components/comment/comment.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AdminComponent } from './mudules/admin/admin.component';
+import { AccountComponent } from './mudules/admin/components/account/account.component';
+import { ViewAppointmentsComponent } from './mudules/admin/components/view-appointments/view-appointments.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,15 @@ import { CommentComponent } from './components/comment/comment.component';
     AboutComponent,
     BookingComponent,
     ProductsComponent,
-    CommentComponent
+    CommentComponent,
+    NavbarComponent,
+    SidebarComponent,
+    AdminComponent,
+    AccountComponent,
+    ViewAppointmentsComponent,
+    ContactComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mudules/admin/components/view-appointments/view-appointments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-appointments',
+  templateUrl: './view-appointments.component.html',
+  styleUrl: './view-appointments.component.css'
+})
+export class ViewAppointmentsComponent {
+
+}
