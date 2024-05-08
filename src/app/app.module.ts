06df318f7c1e2f311e6ce210405a54cd990aae4f
NawodaRanasingha <nawodaranasinha@gmail.com>
2024-05-08 12:33:14 +0530

    my new updated data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BookingComponent,
+    ProductsComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
