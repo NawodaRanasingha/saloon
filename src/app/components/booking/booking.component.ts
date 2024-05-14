1bc844831c51d9046ef570b7054e2b2be4de026e
NawodaRanasingha <nawodaranasinha@gmail.com>
2024-05-14 23:08:55 +0530

    new

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrl: './booking.component.css'
+  styleUrls: ['./booking.component.css']
 })
 export class BookingComponent {
+  bookingDetails = {
+    name: '',
+    email: '',
+    date: ''
+  };
 
+  submitBooking() {
+    // Here you can handle the submission logic, like sending the data to a backend server
+    console.log('Booking submitted:', this.bookingDetails);
+  }
 }
